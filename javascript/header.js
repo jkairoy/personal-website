b5002257d7a98a669fc77a04f985b40bd97ee1a8
Johnny Roy <jkairoy@gmail.com>
2019-02-06 23:33:31 -0500

    initial

---
+++ javascript/header.js
@@ -0,0 +1,54 @@
+$(window).scroll(function() {
+var yPos = window.pageYOffset;
+var yPoint = 870;
+if(yPos > yPoint) {
+   $(".homeHeaderButton").addClass("homeHeaderButtonBlack");
+   $(".homeHeader").css("background-color","white");
+} else {
+   $(".homeHeaderButton").removeClass("homeHeaderButtonBlack");
+   $(".homeHeader").css("background-color","transparent");
+ }
+});
+
+$(document).ready(function (){
+    $("#moreButton").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#profile").offset().top
+        }, 300);
+    });
+});
+$(document).ready(function (){
+    $("#button1").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#home").offset().top
+        }, 400);
+    });
+});
+$(document).ready(function (){
+    $("#button2").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#profile").offset().top
+        }, 400);
+    });
+});
+$(document).ready(function (){
+    $("#button3").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 400);
+    });
+});
+$(document).ready(function (){
+    $("#button4").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#education").offset().top
+        }, 400);
+    });
+});
+$(document).ready(function (){
+    $("#button5").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#portfolio").offset().top
+        }, 400);
+    });
+});
