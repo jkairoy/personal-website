8f4bc419262e7ddd470ccaf473ab08094d499694
jkairoy <jkairoy@gmail.com>
2020-06-02 09:52:47 -0700

    new projects

--- javascript/navigate.js
+++ javascript/navigate.js
@@ -1,49 +1,52 @@
 // navigate
-$(document).ready(function (){
+$(document).ready(function() {
   function navigate(href) {
-    $("body").animate({opacity: 0}, 500)
-    setTimeout(function(){
+    $("body").animate({ opacity: 0 }, 500)
+    setTimeout(function() {
       window.location.href = href;
     }, 500)
   }
-  $("#button1").click(function (){
+  $("#button1").click(function() {
     if ($("#index").length) {
-      $(window).scrollTop($("#work").offset().top-70);
+      $(window).scrollTop($("#work").offset().top - 70);
     } else if ($("#portfolio").length) {
       navigate("../index.html");
     } else {
       navigate("index.html");
     }
   });
-  $("#arrow").click(function (){
-    $(window).scrollTop($("#work").offset().top-70);
+  $("#arrow").click(function() {
+    $(window).scrollTop($("#work").offset().top - 70);
   });
-  $("#button2").click(function (){
+  $("#button2").click(function() {
     if ($("#portfolio").length) {
       navigate("../about.html");
     } else {
       navigate("about.html");
     }
   });
-  $("#buttonvenue").click(function (){
+  $("#buttonvenue").click(function() {
     navigate("portfolio/venue.html");
   });
-  $("#buttonaudioalpaca").click(function (){
+  $("#buttonaudioalpaca").click(function() {
     navigate("portfolio/audioalpaca.html");
   });
-  $("#buttonfullstack").click(function (){
+  $("#buttonfullstack").click(function() {
     navigate("portfolio/fullstack.html");
   });
-  $("#buttontinytodo").click(function (){
+  $("#buttontinytodo").click(function() {
     navigate("portfolio/tinytodo.html");
   });
-  $("#buttonbeenthere").click(function (){
+  $("#buttonbeenthere").click(function() {
     navigate("portfolio/beenthere.html");
   });
-  $("#buttonmylkguys").click(function (){
+  $("#buttonmylkguys").click(function() {
     navigate("portfolio/mylkguys.html");
   });
-  $("#buttonnordson").click(function (){
+  $("#buttonnordson").click(function() {
     navigate("portfolio/nordson.html");
+  });
+  $("#buttonbrownapi").click(function() {
+    navigate("portfolio/brownapi.html");
   });
 });
